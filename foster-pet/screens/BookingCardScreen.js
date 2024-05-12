a5286172d2c3e37a8e67813bf3a61207315ce6a5
Sachin Tharaka <60689075+Sachin-Tharaka@users.noreply.github.com>
2024-05-12 15:34:07 +0530

    change styles (#31)

--- foster-pet/screens/BookingCardScreen.js
+++ foster-pet/screens/BookingCardScreen.js
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Modal, FlatList } from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import PetsService from '../services/PetsService';
-import BookingService from '../services/BookingService';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Modal,
+  FlatList,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Picker } from "@react-native-picker/picker";
+import PetsService from "../services/PetsService";
+import BookingService from "../services/BookingService";
 
 const BookingCardScreen = ({ route, navigation }) => {
-  const { kennelID } = route.params || { kennelID: '' };
-  
-  const [petID, setPetID] = useState('');
-  const [volunteerID, setVolunteerID] = useState('');
+  const { kennelID } = route.params || { kennelID: "" };
+
+  const [petID, setPetID] = useState("");
+  const [volunteerID, setVolunteerID] = useState("");
   const [selectedStartDate, setSelectedStartDate] = useState(new Date());
   const [startTime, setStartTime] = useState(new Date());
   const [selectedEndDate, setSelectedEndDate] = useState(new Date());
@@ -20,22 +28,22 @@ const BookingCardScreen = ({ route, navigation }) => {
   const [showEndTimePicker, setShowEndTimePicker] = useState(false);
   const [numPets, setNumPets] = useState(1);
   const [pets, setPets] = useState([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [showPetModal, setShowPetModal] = useState(false);
 
   useEffect(() => {
     const getToken = async () => {
       try {
-        const token = await AsyncStorage.getItem('token');
-        const ownerID = await AsyncStorage.getItem('userId');
+        const token = await AsyncStorage.getItem("token");
+        const ownerID = await AsyncStorage.getItem("userId");
         if (token) {
           getPetsByOwnerId(ownerID, token);
         } else {
           console.log("Please login");
-          navigation.navigate('Login');
+          navigation.navigate("Login");
         }
       } catch (error) {
-        console.error('Error getting token:', error.message);
+        console.error("Error getting token:", error.message);
       }
     };
     getToken();
@@ -47,80 +55,97 @@ const BookingCardScreen = ({ route, navigation }) => {
       const data = await PetsService.getPetsByOwnerId(id, token);
       setPets(data);
     } catch (error) {
-      console.error('Error:', error.message);
+      console.error("Error:", error.message);
     }
   };
 
   const handleBooking = async () => {
     setError(" ");
     if (!petID) {
-      setError('Pet is required.');
+      setError("Pet is required.");
       return;
     } else if (!selectedStartDate) {
-      setError('Start date is required.');
+      setError("Start date is required.");
       return;
     } else if (!startTime) {
-      setError('Start time is required.');
+      setError("Start time is required.");
       return;
     } else if (!selectedEndDate) {
-      setError('End date is required.');
+      setError("End date is required.");
       return;
     } else if (!endTime) {
-      setError('End time is required.');
+      setError("End time is required.");
       return;
     }
- 
-    try {
-      const token = await AsyncStorage.getItem('token');
-      const ownerID = await AsyncStorage.getItem('userId');
-      
-      console.log('petid: ',petID,'ownerid: ', ownerID,'kennelid: ', kennelID,'volunteerid: ', volunteerID, 'start date: ',startDate,'end date: ', endDate, token);
-      const responseData = await BookingService.booking(petID, ownerID, kennelID, volunteerID, startDate, endDate, token);
-      console.log('Booking completed:', responseData);
 
+    try {
+      const token = await AsyncStorage.getItem("token");
+      const ownerID = await AsyncStorage.getItem("userId");
+
+      console.log(
+        "petid: ",
+        petID,
+        "ownerid: ",
+        ownerID,
+        "kennelid: ",
+        kennelID,
+        "volunteerid: ",
+        volunteerID,
+        "start date: ",
+        startDate,
+        "end date: ",
+        endDate,
+        token
+      );
+      const responseData = await BookingService.booking(
+        petID,
+        ownerID,
+        kennelID,
+        volunteerID,
+        startDate,
+        endDate,
+        token
+      );
+      console.log("Booking completed:", responseData);
     } catch (error) {
-      console.error('Booking failed:', error.message);
+      console.error("Booking failed:", error.message);
       setError("Booking failed");
     }
   };
 
-  const startDate = selectedStartDate ? `${selectedStartDate.toISOString().split('T')[0]}T${startTime.toISOString().split('T')[1]}` : '';
-  const endDate = selectedEndDate ? `${selectedEndDate.toISOString().split('T')[0]}T${endTime.toISOString().split('T')[1]}` : '';
+  const startDate = selectedStartDate
+    ? `${selectedStartDate.toISOString().split("T")[0]} ${startTime
+        .toISOString()
+        .split("T")[1]
+        .slice(0, 5)}`
+    : "";
+
+  const endDate = selectedEndDate
+    ? `${selectedEndDate.toISOString().split("T")[0]} ${endTime
+        .toISOString()
+        .split("T")[1]
+        .slice(0, 5)}`
+    : "";
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Booking Card</Text>
       {error && <Text style={styles.error}>{error}</Text>}
-      
-      <TouchableOpacity onPress={() => setShowPetModal(true)} style={styles.button}>
-        <Text>{petID ? pets.find(pet => pet.petID === petID).petName : 'Select Pet'}</Text>
-      </TouchableOpacity>
-      <Modal
-        visible={showPetModal}
-        transparent
-        animationType="slide"
-        onRequestClose={() => setShowPetModal(false)}
-      >
-        <TouchableOpacity style={styles.modalBackground} onPress={() => setShowPetModal(false)}>
-          <FlatList
-            data={pets}
-            keyExtractor={(item) => item.petID.toString()}
-            renderItem={({ item }) => (
-              <TouchableOpacity
-                style={styles.option}
-                onPress={() => {
-                  setPetID(item.petID);
-                  setShowPetModal(false);
-                }}
-              >
-                <Text>{item.petName}</Text>
-              </TouchableOpacity>
-            )}
-          />
-        </TouchableOpacity>
-      </Modal>
 
-      <TouchableOpacity onPress={() => setShowStartDatePicker(true)} style={styles.button}>
+      <Picker
+        selectedValue={petID}
+        onValueChange={(itemValue, itemIndex) => setPetID(itemValue)}
+        style={styles.picker}
+      >
+        <Picker.Item label="Select Pet" value="" />
+        {pets.map((pet) => (
+          <Picker.Item key={pet.petID} label={pet.petName} value={pet.petID} />
+        ))}
+      </Picker>
+      <TouchableOpacity
+        onPress={() => setShowStartDatePicker(true)}
+        style={styles.setterButton}
+      >
         <Text>Select Start Date</Text>
       </TouchableOpacity>
       {showStartDatePicker && (
@@ -138,7 +163,10 @@ const BookingCardScreen = ({ route, navigation }) => {
         />
       )}
 
-      <TouchableOpacity onPress={() => setShowStartTimePicker(true)} style={styles.button}>
+      <TouchableOpacity
+        onPress={() => setShowStartTimePicker(true)}
+        style={styles.setterButton}
+      >
         <Text>Select Start Time</Text>
       </TouchableOpacity>
       {showStartTimePicker && (
@@ -158,7 +186,10 @@ const BookingCardScreen = ({ route, navigation }) => {
 
       <Text>Start Date and Time: {startDate}</Text>
 
-      <TouchableOpacity onPress={() => setShowEndDatePicker(true)} style={styles.button}>
+      <TouchableOpacity
+        onPress={() => setShowEndDatePicker(true)}
+        style={styles.setterButton}
+      >
         <Text>Select End Date</Text>
       </TouchableOpacity>
       {showEndDatePicker && (
@@ -176,7 +207,10 @@ const BookingCardScreen = ({ route, navigation }) => {
         />
       )}
 
-      <TouchableOpacity onPress={() => setShowEndTimePicker(true)} style={styles.button}>
+      <TouchableOpacity
+        onPress={() => setShowEndTimePicker(true)}
+        style={styles.setterButton}
+      >
         <Text>Select End Time</Text>
       </TouchableOpacity>
       {showEndTimePicker && (
@@ -196,11 +230,17 @@ const BookingCardScreen = ({ route, navigation }) => {
       <Text>End Date and Time: {endDate}</Text>
       <View style={styles.counterContainer}>
         <Text>Number of Pets: </Text>
-        <TouchableOpacity onPress={() => setNumPets(Math.max(1, numPets - 1))} style={styles.counterButton}>
+        <TouchableOpacity
+          onPress={() => setNumPets(Math.max(1, numPets - 1))}
+          style={styles.counterButton}
+        >
           <Text>-</Text>
         </TouchableOpacity>
         <Text> {numPets} </Text>
-        <TouchableOpacity onPress={() => setNumPets(numPets + 1)} style={styles.counterButton}>
+        <TouchableOpacity
+          onPress={() => setNumPets(numPets + 1)}
+          style={styles.counterButton}
+        >
           <Text>+</Text>
         </TouchableOpacity>
       </View>
@@ -215,46 +255,61 @@ const BookingCardScreen = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
   },
   text: {
-    fontSize: 26,
-    fontWeight: 'bold',
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
     marginBottom: 20,
-  },
-  button: {
-    padding: 10,
-    marginBottom: 20,
-    backgroundColor: 'lightblue',
-  },
-  modalBackground: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    paddingTop:250,
-    paddingLeft:50,
-    paddingRight:50
-  },
-  option: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: 'lightgray',
-    backgroundColor: 'white',
-  },
-  counterContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  counterButton: {
-    borderWidth: 1,
-    padding: 5,
   },
   error: {
-    color: 'red',
-    marginTop: 5,
+    fontSize: 16,
+    color: "red",
     marginBottom: 10,
+  },
+  picker: {
+    width: "90%",
+    marginVertical: 10,
+    height: 40,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "#007bff",
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 10,
+    width: "90%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  setterButton: {
+    backgroundColor: "#E0E0E0",
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 10,
+    width: "90%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  counterContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  counterButton: {
+    backgroundColor: "#e0e0e0",
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    marginHorizontal: 10,
+    borderRadius: 5,
   },
 });
 
